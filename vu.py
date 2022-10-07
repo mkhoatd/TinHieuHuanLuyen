5d9263144bef4524c8a7118aec8b0253bad522b2
mkhoatd <binnhatle@gmail.com>
2022-10-07 15:19:40 +0700

    abc

--- vu.py
+++ vu.py
@@ -2,7 +2,7 @@
 import librosa.display
 import librosa
 import numpy as np
-import plotly.express as px
+import plotly.graph_objects as go
 
 # speech-silence and voice-unvoiced
 def read_lab(lab_file_name: str):
@@ -20,33 +20,55 @@ def get_closest(array, values):
     return array[idx]
 
 
-# %%
-print(read_lab('./studio_F2.lab'))
+def array_norm(arr: np.ndarray):
+    arr=np.copy(arr)
+    max=np.max(arr)
+    min=np.min(arr)
+    return (arr-min)/(max-min)
+
 
 # %%
-signal, sr= librosa.load('./phone_F2.wav')
+signal, sr= librosa.load('./studio_F2.wav')
 # librosa.display.waveshow(signal, sr=sr)
 t_i = 0
 t_f = signal.shape[0] / sr
 t = np.linspace(t_i, t_f, num=signal.shape[0])
-fig=px.line(x=t, y=signal)
+# fig=px.line(x=t, y=signal)
+# fig.show()
+# %%
+# Calculate STE
+window_len=490
+STE=np.array([np.sum(np.square(signal[i:i+window_len]))/np.ones(window_len) for i in range(0, len(signal), window_len)])
+STE=STE.reshape(-1)
+STE=array_norm(STE)
+
+
+# %%
+# Calculate MA
+MA=np.array([np.sum(np.abs(signal[i:i+window_len]))/np.ones(window_len) for i in range(0, len(signal), window_len)])
+MA=MA.reshape(-1)
+MA=array_norm(MA)
+# %%
+fig=go.Figure()
+fig.add_trace(go.Scatter(x=t, y=signal, name='signal'))
+fig.add_trace(go.Scatter(x=t, y=STE, name='STE'))
+fig.add_trace(go.Scatter(x=t, y=MA, name='MA'))
+fig.update_layout(
+    title="Phone_F2"
+)
 fig.show()
-# %%
-
-
 
 # %%
-samples_per_milis=sr/100
-
-
-# e = STE(phone_f2[0])
-# fig, ax = plt.subplots(1, 1)
-# ax.plot(t[10000:11000], phone_f2[0][10000:11000])
-# ax.plot(t[50000:70000], e[50000:70000], ':r')
-# ax.plot(t, e, ':b')
-
-# plt.show()
+data=read_lab('./phone_F2.lab')
+mean_std=data[-2:]
+data=data[:-2]
+data_v_uv=list(filter(lambda x:(x[2]=='v' or x[2]=='uv'), data))
+print(data_v_uv)
 
 
 
+
+# %%
+x=data[1]
+(x[2]=='v') or (x[2]=='uv')
 # %%
